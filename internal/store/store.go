6e739cc4311e929a497800ed9939619554bb9e88
Miguel <nullcarter@gmail.com>
2025-07-29 21:28:24 -0500

    chore(users): create initialization func for userstore

--- internal/store/store.go
+++ internal/store/store.go
@@ -3,10 +3,12 @@ package store
 import (
 	"context"
 	"database/sql"
+	"log"
 )
 
 type Store struct {
 	User interface {
+		Initialize() error
 		Create(context.Context) error
 		Get(context.Context) error
 		Update(context.Context) error
@@ -15,7 +17,14 @@ type Store struct {
 }
 
 func NewStore(db *sql.DB) Store {
+	userStore := &UserStore{db}
+	err := userStore.Initialize()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	return Store{
-		User: &UserStore{db},
+		User: userStore,
 	}
 }
--- internal/store/user.go
+++ internal/store/user.go
@@ -3,10 +3,39 @@ package store
 import (
 	"context"
 	"database/sql"
+	"fmt"
 )
+
+type User struct {
+	id          int
+	username    string
+	displayName string
+}
 
 type UserStore struct {
 	db *sql.DB
+}
+
+func (us *UserStore) Initialize() error {
+	_, err := us.db.ExecContext(context.Background(),
+		`
+		CREATE TABLE IF NOT EXISTS users (
+  			id INTEGER PRIMARY KEY AUTOINCREMENT,
+     		username TEXT UNIQUE NOT NULL,
+       		display_name TEXT,
+         	avatar_url TEXT,
+          	created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+        );
+		`)
+
+	if err != nil {
+		fmt.Printf("user table: %s", err)
+		return err
+	}
+
+	fmt.Println("users table initialized.")
+
+	return nil
 }
 
 func (us *UserStore) Create(ctx context.Context) error {
